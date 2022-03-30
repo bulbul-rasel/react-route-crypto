09c523d0410da01b4cd2eeac858556b25e79c85a
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-03-30 20:29:10 +0600

    nested route & not found Page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Footer from './components/Footer/Footer'
 import HomePage from './components/Homepage/Homepage'
 import Coins from './components/Coins/Coins'
 import Contact from './components/Contact/Contact'
+import BdAddress from './components/Contact/BdAddress'
+import UsAddress from './components/Contact/UsAddress'
 import About from './components/About/About'
+import NotFound from './components/NotFound/NotFound'
 import CoinDetails from './components/CoinDetails/CoinDetails'
 
 const App = () => {
@@ -16,8 +19,12 @@ const App = () => {
         <Route path='/' element={<HomePage></HomePage>}></Route>
         <Route path='/coins' element={<Coins />}></Route>
         <Route path='/coin-details/:id' element={<CoinDetails />}></Route>
-        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/contact' element={<Contact />}>
+          <Route path='bd-address' element={<BdAddress></BdAddress>}></Route>
+          <Route path='us-address' element={<UsAddress></UsAddress>}></Route>
+        </Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='/*' element={<NotFound />}></Route>
       </Routes>
       <Footer></Footer>
     </div>
