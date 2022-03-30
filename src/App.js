b2b84da69854f68530cc3b56e299f84ad80c211e
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-03-30 20:09:31 +0600

    loade coin details

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from './components/Footer/Footer'
 import HomePage from './components/Homepage/Homepage'
 import Coins from './components/Coins/Coins'
 import Contact from './components/Contact/Contact'
+import About from './components/About/About'
+import CoinDetails from './components/CoinDetails/CoinDetails'
 
 const App = () => {
   return (
@@ -13,7 +15,9 @@ const App = () => {
       <Routes>
         <Route path='/' element={<HomePage></HomePage>}></Route>
         <Route path='/coins' element={<Coins />}></Route>
+        <Route path='/coin-details/:id' element={<CoinDetails />}></Route>
         <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/about' element={<About />}></Route>
       </Routes>
       <Footer></Footer>
     </div>
